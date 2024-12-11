b7b719625d491b15feafc4c00861cd8fde9b0e35
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2024-12-11 17:31:12 +0100

    Added dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Location from "./(components)/location";
+import { ModeToggle } from "./(components)/ui/dark-mode-toggle";
 
 export default function Home() {
   return (
     <div>
+      <div className="absolute right-4 top-4">
+        <ModeToggle />
+      </div>
       <Location />
     </div>
   );
